21c86309937d7529ec9da8111092eecfdf1a50d2
NikitaSoshnikov <72586065+NikitaSoshnikov@users.noreply.github.com>
2020-12-21 18:51:13 -0800

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,29 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.23,
+            length: 450
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly() {
+        this.sling.bodyA = null
+    }
+    display(){
+        if (this.sling.bodyA) {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB
+            strokeWeight(4);
+              
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        
+        }
+        
+        
+    }
+    
+}
